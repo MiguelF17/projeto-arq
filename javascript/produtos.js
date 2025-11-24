d267730d96c1a1be4ff1f7711185804d4b02306b
MiguelF17 <miguelfrancellino1705@gmail.com>
2025-11-24 08:11:24 -0300

    adicionando ids nos produtos

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -4,6 +4,7 @@
 const produtos = [
     // -------- Sala de Estar / Quarto --------
     {
+        id: 1,
         nome: "Sofá Cor Cinza Moderno",
         preco: 2120.55,
         categoria: "Sala de Estar",
@@ -12,6 +13,7 @@ const produtos = [
         imagem: "../style/imgs/sofacinza.png"
     },
     {
+        id: 2,
         nome: "Poltronas Bege e Puff",
         preco: 576.79,
         categoria: "Sala de Estar",
@@ -20,6 +22,7 @@ const produtos = [
         imagem: "../style/imgs/poutronas.png"
     },
     {
+        id: 3,
         nome: "Conjunto Cabeceiras",
         preco: 242.45,
         categoria: "Quarto",
@@ -28,6 +31,7 @@ const produtos = [
         imagem: "../style/imgs/ccabeceiras.png"
     },
     {
+        id: 4,
         nome: "Poltrona Bege Simples",
         preco: 562.32,
         categoria: "Sala de Estar",
@@ -36,6 +40,7 @@ const produtos = [
         imagem: "../style/imgs/poltrona.png"
     },
     {
+        id: 5,
         nome: "Cama Bege Moderna",
         preco: 2360.79,
         categoria: "Quarto",
@@ -44,6 +49,7 @@ const produtos = [
         imagem: "../style/imgs/camacinza.png"
     },
     {
+        id: 6,
         nome: "Poltrona Bege Atual",
         preco: 662.32,
         categoria: "Sala de Estar",
@@ -52,6 +58,7 @@ const produtos = [
         imagem: "../style/imgs/poutrona2.png"
     },
     {
+        id: 7,
         nome: "Lustre Luminária",
         preco: 835.36,
         categoria: "Decoração",
@@ -60,6 +67,7 @@ const produtos = [
         imagem: "../style/imgs/luminaria.png"
     },
     {
+        id: 8,
         nome: "Sofá Cinza Seccional",
         preco: 1002.02,
         categoria: "Sala de Estar",
@@ -68,6 +76,7 @@ const produtos = [
         imagem: "../style/imgs/sofa.png"
     },
     {
+        id: 9,
         nome: "Cabeceira Murat Preta",
         preco: 662.32,
         categoria: "Quarto",
@@ -78,6 +87,7 @@ const produtos = [
 
     // -------- Cozinha e Jantar --------
     {
+        id: 10,
         nome: "Banco Chique Branco",
         preco: 420.55,
         categoria: "Sala de Jantar",
@@ -86,6 +96,7 @@ const produtos = [
         imagem: "../style/imgs/banco.png"
     },
     {
+        id: 11,
         nome: "Mesa de Janta Moderna",
         preco: 976.79,
         categoria: "Sala de Jantar",
@@ -94,6 +105,7 @@ const produtos = [
         imagem: "../style/imgs/mesa de janta.png"
     },
     {
+        id: 12,
         nome: "Armário de Cozinha",
         preco: 742.45,
         categoria: "Cozinha",
@@ -102,6 +114,7 @@ const produtos = [
         imagem: "../style/imgs/amário.png"
     },
     {
+        id: 13,
         nome: "Panelas e Frigideiras",
         preco: 642.12,
         categoria: "Cozinha",
@@ -110,6 +123,7 @@ const produtos = [
         imagem: "../style/imgs/panelas.png"
     },
     {
+        id: 14,
         nome: "Escorredor Moderno",
         preco: 159.99,
         categoria: "Cozinha",
@@ -118,6 +132,7 @@ const produtos = [
         imagem: "../style/imgs/escorredor.png"
     },
     {
+        id: 15,
         nome: "Talheres Modernos",
         preco: 32.32,
         categoria: "Cozinha",
@@ -126,6 +141,7 @@ const produtos = [
         imagem: "../style/imgs/talheres.png"
     },
     {
+        id: 16,
         nome: "Pratos de Porcelana",
         preco: 135.36,
         categoria: "Cozinha",
@@ -134,6 +150,7 @@ const produtos = [
         imagem: "../style/imgs/pratos.png"
     },
     {
+        id: 17,
         nome: "Conjunto de Utensílios",
         preco: 32.32,
         categoria: "Cozinha",
@@ -144,6 +161,7 @@ const produtos = [
 
     // -------- Banheiro --------
     {
+        id: 18,
         nome: "Suporte para Toalhas",
         preco: 35.55,
         categoria: "Banheiro",
@@ -152,6 +170,7 @@ const produtos = [
         imagem: "../style/imgs/suporte de toalhas.png"
     },
     {
+        id: 19,
         nome: "Banqueira Moderna",
         preco: 46.79,
         categoria: "Banheiro",
@@ -160,6 +179,7 @@ const produtos = [
         imagem: "../style/imgs/bamqueira.png"
     },
     {
+        id: 20,
         nome: "Banheira Moderna",
         preco: 842.45,
         categoria: "Banheiro",
@@ -168,6 +188,7 @@ const produtos = [
         imagem: "../style/imgs/banheira.png"
     },
     {
+        id: 21,
         nome: "Box Lateral Moderno",
         preco: 562.32,
         categoria: "Banheiro",
@@ -176,6 +197,7 @@ const produtos = [
         imagem: "../style/imgs/box.png"
     },
     {
+        id: 22,
         nome: "Espelho Circular",
         preco: 60.79,
         categoria: "Banheiro",
@@ -184,6 +206,7 @@ const produtos = [
         imagem: "../style/imgs/espelho.png"
     },
     {
+        id: 23,
         nome: "Pia Preta Moderna",
         preco: 74.32,
         categoria: "Banheiro",
@@ -192,6 +215,7 @@ const produtos = [
         imagem: "../style/imgs/pia.png"
     },
     {
+        id: 24,
         nome: "Torneira Preta para Pia",
         preco: 28.36,
         categoria: "Banheiro",
@@ -200,6 +224,7 @@ const produtos = [
         imagem: "../style/imgs/torneira.png"
     },
     {
+        id: 25,
         nome: "Gabinete de Pia",
         preco: 52.32,
         categoria: "Banheiro",
